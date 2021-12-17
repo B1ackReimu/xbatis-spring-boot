c6dfa8b9b0f4144289dedf446de2ebe63107f7d7
BlackReimu <BlackReimu@2333.com>
2021-12-17 16:38:49 +0800

    jifoacsjco

--- xbatis-spring-boot-autoconfigure/src/main/java/org/xbatis/spring/boot/autoconfigure/TestConfiguration.java
+++ xbatis-spring-boot-autoconfigure/src/main/java/org/xbatis/spring/boot/autoconfigure/TestConfiguration.java
@@ -1,18 +1,29 @@
 package org.xbatis.spring.boot.autoconfigure;
 
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConditionalOnBean(A.class)
-public class TestConfiguration {
+public class TestConfiguration implements ApplicationContextAware {
 
     public TestConfiguration(@Qualifier(value = "b") A a) {
         System.out.println("TestConfiguration:" + a.getClassName());
     }
 
 
-
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        ConfigurableApplicationContext context = (ConfigurableApplicationContext) applicationContext;
+        BeanDefinitionRegistry beanDefinitionRegistry = (BeanDefinitionRegistry) context.getBeanFactory();
+        beanDefinitionRegistry.removeBeanDefinition("b");
+    }
 }
 
