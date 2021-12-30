cc619b8b56db06842b9fbd98fcb53500f0a0c240
BlackReimu <blackreimu@toho.xyz>
2021-12-30 21:45:30 +0800

    ZXCXCC

--- xbatis-spring-boot-autoconfigure/src/main/java/org/xbatis/spring/boot/config/XbatisConfig.java
+++ xbatis-spring-boot-autoconfigure/src/main/java/org/xbatis/spring/boot/config/XbatisConfig.java
@@ -3,14 +3,27 @@ package org.xbatis.spring.boot.config;
 import java.util.HashSet;
 public final class XbatisConfig {
 
-    private HashSet<XbatisNamespace> namespaces;
+    private final HashSet<XbatisNamespace> namespaces = new HashSet<>();
 
     public XbatisNamespace addNamespace(String name){
+        XbatisNamespace xbatisNamespace = new XbatisNamespace(name);
+        namespaces.add(xbatisNamespace);
+        return xbatisNamespace;
     }
 
     private class XbatisNamespace {
         private String name;
         private HashSet<XbatisGroup> groups;
+
+        public XbatisNamespace(String name) {
+            this.name = name;
+        }
+
+        public XbatisGroup addGroup(String name){
+            XbatisGroup xbatisGroup = new XbatisGroup();
+            groups.add(xbatisGroup);
+            return xbatisGroup;
+        }
 
         private class XbatisGroup {
             private String name;
